02991b0311bd7823270f531fcca38aa95d878c24
Cristiano Balducci <cristiano.balducci@gmail.com>
2016-01-08 16:51:15 +0100

    Implemented method basic http communication

--- lib/feynman.rb
+++ lib/feynman.rb
@@ -1,30 +1,56 @@
 require "./feynman/version"
 require "socket"
-require "uri"
+require "net/http"
+require "yaml"
 
 module Feynman
 
+  class Config
+
+    attr_reader :config, :local_port, :target_host, :target_port, :docker_host, :docker_port, :docker_ssl
+
+    def initialize(c="feynman.yml")
+      config = File.open(c) { |y| YAML::load(y)}
+      @local_port = config["local"]["port"]
+      @target_host = config["target"]["host"]
+      @target_port = config["target"]["port"]
+      @docker_host = config["docker"]["host"]
+      @docker_port = config["docker"]["port"]
+      @docker_ssl = config["docker"]["ssl"]
+    end
+  end
+
   class FProxy
+
     def start(port)
       puts "Creating listening TCP socket on #{port}"
       @sock=TCPServer.open(port)
       loop {
         Thread.start(@sock.accept) { |c| handle(c) }
       }
+    rescue Interrupt
+      puts "Interrupt received"
+    ensure
+      @sock.close if @sock
+      puts "Gracefully dying"
     end
 
     def handle(client)
-      loop {
-        to_server=TCPSocket.new("127.0.0.1","4567")
-        request = client.readline
-        puts "Asking for #{request}"
-        to_server.write(request)
-        response = to_server.read
-        puts "Got answer #{response}"
-      }
+      puts "Accepted connection from #{client}"
+      uri = URI("http://127.0.0.1:4567/movies")
+      Net::HTTP.start(uri.host, uri.port) do |h|
+        req = Net::HTTP::Get.new(uri)
+        res = h.request(req)
+        client.write("HTTP/#{res.http_version} #{res.code} #{res.message}\n")
+        client.write("Content-type: #{res.content_type}\n")
+        client.write("Content-length: #{res.content_length}\n\n")
+        client.write("#{res.body}")
+        client.close
+      end
     end
   end
 
+config = Config.new(ARGV.shift || "feynman.yml")
 proxy = FProxy.new
-proxy.start(1099)
+proxy.start(config.local_port)
 end
